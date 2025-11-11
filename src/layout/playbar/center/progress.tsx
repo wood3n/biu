c1c7a846fcb9ab42e8bfd1f45bc90e291a280edb
wood3n <wangkka1@163.com>
2025-11-11 21:37:55 +0800

    fix: ui style

--- src/layout/playbar/center/progress.tsx
+++ src/layout/playbar/center/progress.tsx
@@ -14,48 +14,35 @@ const Progress = ({ isDisabled }: SliderProps) => {
   const showThumb = hovered && !isDisabled;
 
   return (
-    <Slider
-      aria-label="播放进度"
-      hideThumb
-      minValue={0}
-      maxValue={duration || 0}
-      value={currentTime}
-      onChange={v => seek(v as number)}
-      isDisabled={isDisabled}
-      size="sm"
-      disableThumbScale
-      color={showThumb ? "primary" : "foreground"}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      renderThumb={thumbProps =>
-        showThumb && (
-          <div
-            {...thumbProps}
-            className="group bg-background shadow-medium top-1/2 cursor-grab rounded-full data-[dragging=true]:cursor-grabbing"
-          >
-            <span className="shadow-small block h-4 w-4 rounded-full bg-white" />
-          </div>
-        )
-      }
-      startContent={
-        <div className="flex w-12 justify-center text-sm whitespace-nowrap opacity-70">
-          {currentTime ? formatDuration(currentTime) : "-:--"}
-        </div>
-      }
-      endContent={
-        <span className="flex w-12 justify-center text-sm whitespace-nowrap opacity-70">
-          {duration ? formatDuration(duration) : "-:--"}
-        </span>
-      }
-      className={clx({
-        "w-3/4": true,
-        "cursor-pointer": !isDisabled,
-        "cursor-default": isDisabled,
-      })}
-      classNames={{
-        track: "h-[4px]",
-      }}
-    />
+    <div className="flex w-3/4 items-center space-x-2">
+      <div className="flex justify-center text-sm whitespace-nowrap opacity-70">
+        {currentTime ? formatDuration(currentTime) : "-:--"}
+      </div>
+      <Slider
+        aria-label="播放进度"
+        hideThumb={!showThumb}
+        minValue={0}
+        maxValue={duration || 0}
+        value={currentTime}
+        onChange={v => seek(v as number)}
+        isDisabled={isDisabled}
+        size="sm"
+        color={showThumb ? "primary" : "foreground"}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className={clx("flex-1", {
+          "cursor-pointer": !isDisabled,
+          "cursor-not-allowed": isDisabled,
+        })}
+        classNames={{
+          track: "h-[4px]",
+          thumb: "after:bg-primary",
+        }}
+      />
+      <span className="flex justify-center text-sm whitespace-nowrap opacity-70">
+        {duration ? formatDuration(duration) : "-:--"}
+      </span>
+    </div>
   );
 };
 
